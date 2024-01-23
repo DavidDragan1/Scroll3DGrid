d9f4198c59b5cf2070d5a097caad3fa94e9af21b
DavidDragan1 <Daviddragan@protonmail.com>
2024-01-23 23:05:01 +0000

    Update index.js

--- js/index.js
+++ js/index.js
@@ -53,7 +53,7 @@ const applyAnimation = (grid, animationType) => {
 			// Set some CSS related style values
 			grid.style.setProperty('--perspective', '500px');
 			grid.style.setProperty('--grid-width', '400%');
-			grid.style.setProperty('--grid-columns', '15');
+			grid.style.setProperty('--grid-columns', '25');
 			grid.style.setProperty('--grid-gap', '0.2');
 			
 			
