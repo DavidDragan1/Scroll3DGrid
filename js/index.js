89cb537b27b32ad02fc9afc55d36dd12a0cff21d
DavidDragan1 <Daviddragan@protonmail.com>
2024-01-23 23:13:56 +0000

    Update index.js

--- js/index.js
+++ js/index.js
@@ -60,7 +60,7 @@ const applyAnimation = (grid, animationType) => {
 			const gridObj = getGrid(gridItems);
 
 			timeline
-			.set(gridWrap, {rotationX: 65, rotationY: 320, rotationZ: 0})
+			.set(gridWrap, {rotationX: 65, rotationY: 350, rotationZ: 0})
 			
 			.to(gridObj.rows('even'), {
 				xPercent: -200,
